0b38c28762b9e5b6ca2e4b4658117c41b63e89c5
Javier Alonso <jalonso@teldat.com>
2022-11-02 16:09:47 +0100

    Ref T12562#OCID 814## Plugins: initial plugin structure for Orcha v0.3.0
    This commit defines the basic structure a plugin should have, as well as a
    basic plugin (+ pluggable) that interacts with SystemD Watchdog

---
+++ watchdog/setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="orcha-watchdog",
+    version="0.1.3",
+    packages=find_packages(),
+    url="",
+    license="",
+    author="Javinator9889",
+    author_email="jalonso@teldat.com",
+    description="SystemD Watchdog pluggable for Orcha",
+    zip_safe=True,
+    python_requires=">=3.7",
+)
